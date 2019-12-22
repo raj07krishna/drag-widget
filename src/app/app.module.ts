3b4b234ef684408da723de034c9cf7b484d31060
Raj krishna <raj07krishna@gmail.com>
2019-12-22 21:44:02 +0530

    box ready with resize cursors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WidgetBoxComponent } from './widget-box/widget-box.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WidgetBoxComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/widget-box/widget-box.model.ts
@@ -0,0 +1,9 @@
+export interface widgetBox {
+    order: number
+    width: number
+    height: number
+    rowPosition: number
+    colPosition: number
+    isDragging: boolean
+    isResizing: boolean
+}
